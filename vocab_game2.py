aefac2209451ff6c6ed4aa9107e435ce155786aa
412-06-nacharot <54229@yupparaj.ac.th>
2026-09-09 14:57:06 +0700

    Update vocab_game2.py

--- vocab_game2.py
+++ vocab_game2.py
@@ -46,14 +46,14 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
 
   
-    if u_ans3 == "black":
+    if u_ans3 == "purple":
         st.success("✅ ข้อ 3: ถูกต้อง")
         score += 1
     else:
         st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
 
 
-    if u_ans4 == "spain":
+    if u_ans4 == "tiger":
         st.success("✅ ข้อ 4: ถูกต้อง")
         score += 1
     else:
